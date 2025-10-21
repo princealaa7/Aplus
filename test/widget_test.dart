666523bf585d9708076d6f2b3347fbd7d4058fb3
princealaa7 <princealaa777@gmail.com>
2025-10-21 23:50:24 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,32 @@
-// هذا الكود يتجاهل الاختبار بالكامل لضمان نجاح البناء وتجاوز مشاكل Firebase
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:a9/main.dart';
+
 void main() {
-  // استخدام دالة skip: true لتجاهل كل الاختبارات في هذا الملف
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // الاختبار تم تجاهله بنجاح لتجاوز مشاكل Firebase
-  }, skip: true); 
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
 }
+
+هذا الكود هل فيه مشكله معينه ام لا
