3c87e9722f8bdf550d6b6aeb2c51f10f94c88858
princealaa7 <princealaa777@gmail.com>
2025-10-22 00:34:59 +0300

    Add test for app creation
    Added a test to verify that the app can be created without errors.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:aplus/main.dart';
 
 void main() {
   test('Basic app test - always passes', () {
-    // هذا اختبار بسيط ينجح دائماً
-    expect(1, 1); // 1 يساوي 1 - دائماً صحيح
+    expect(1, 1);
+  });
+
+  test('App can be created', () {
+    // تأكد أن التطبيق يمكن إنشاؤه بدون أخطاء
+    expect(() => main(), returnsNormally);
   });
 }
